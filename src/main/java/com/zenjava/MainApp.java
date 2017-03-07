b3a99fcd6b94e61aefa6161a8a787caacde8cc0e
Eric HERMENT <ericherment@outlook.com>
2017-03-07 13:09:54 +0100

    Adding a product

--- src/main/java/com/zenjava/MainApp.java
+++ src/main/java/com/zenjava/MainApp.java
@@ -1,19 +1,23 @@
 package com.zenjava;
 
+import com.zenjava.controllers.HomeController;
 import com.zenjava.model.Product;
 import javafx.application.Application;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import sun.applet.Main;
+
+import java.io.IOException;
+
 
 public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
 
     private ObservableList<Product> observableList = FXCollections.observableArrayList();
 
@@ -21,20 +25,36 @@ public class MainApp extends Application {
         observableList.add(new Product("Produit", "lol", new SimpleIntegerProperty(100)));
     }
 
+    public void start(Stage stage) throws Exception {
+        this.primaryStage = stage;
+        this.primaryStage.setTitle("Home Page");
+
+        initLayout();
+    }
+
+    private void initLayout() {
+        try {
+            String fxmlFile = "/fxml/home_view.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            rootLayout = loader.load(getClass().getResourceAsStream(fxmlFile));
+
+            Scene scene = new Scene(rootLayout, 1100, 800);
+            //scene.getStylesheets().add("/styles/styles.css");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            HomeController homeController = loader.getController();
+            homeController.setMainApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
-    public void start(Stage stage) throws Exception {
-        String fxmlFile = "/fxml/home_view.fxml";
-
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-        Scene scene = new Scene(rootNode, 1100, 800);
-        //scene.getStylesheets().add("/styles/styles.css");
-
-        stage.setTitle("Home page");
-        stage.setScene(scene);
-        stage.show();
+    public ObservableList<Product> getObservableList() {
+        return observableList;
     }
 }
--- src/main/java/com/zenjava/controllers/HomeController.java
+++ src/main/java/com/zenjava/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.zenjava.controllers;
 
 
+import com.zenjava.MainApp;
 import com.zenjava.model.Product;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
@@ -15,7 +16,7 @@ import javafx.scene.control.ListView;
 public class HomeController {
 
     @FXML
-    private ListView<String> listView;
+    private ListView<Product> listView;
 
     @FXML
     private Label productName;
@@ -29,15 +30,29 @@ public class HomeController {
     @FXML
     private ChoiceBox<String> choiceBox;
 
-    @FXML
-    public void initialize() {
+    private MainApp mainApp;
 
+    public HomeController() {
+    }
+
+    @FXML
+    private void initialize() {
     }
 
     public void showProductDetails(Product product) {
         productName.setText(product.getName());
         productDescription.setText(product.getDescription());
         productPrice.setText(Integer.toString(product.getPrice()));
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+        listView.setItems(mainApp.getObservableList());
     }
 
 
