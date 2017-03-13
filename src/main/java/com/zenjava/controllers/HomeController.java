bbf8ce0a36170075ffbceeb17fb6e930a3920336
Guillaume Derringer <guillaume.derringer@etu.unice.fr>
2017-03-13 23:57:15 +0100

    contact button

--- src/main/java/com/zenjava/controllers/HomeController.java
+++ src/main/java/com/zenjava/controllers/HomeController.java
@@ -6,11 +6,18 @@ import com.zenjava.model.Product;
 import com.zenjava.util.ProductCellFactory;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Controller of the home page of the store
@@ -34,6 +41,9 @@ public class HomeController {
 
     @FXML
     private ImageView productImage;
+
+    @FXML
+    private Button contactButton;
 
     private MainApp mainApp;
 
@@ -77,5 +87,17 @@ public class HomeController {
         listView.setCellFactory(new ProductCellFactory());
         choiceBox.setItems(mainApp.getChoiceList());
         choiceBox.setValue(mainApp.getChoiceList().get(0));
+    }
+    @FXML
+    public void handleButtonAction(ActionEvent event) throws IOException {
+        String fxmlFile = "/fxml/contact_view.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Scene scene2 = new Scene(loader.load(getClass().getResourceAsStream(fxmlFile)),1200,800);
+        Stage stage = new Stage();
+        stage.setTitle("contact");
+        stage.setScene(scene2);
+        if (event.getSource()==contactButton){
+            stage.show();
+        }
     }
 }
